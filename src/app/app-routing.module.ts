42cd2829f8b1b78b98e4b186f533055336025de7
Nicolas Fourzans <nicolasfourzans@Nicolass-MacBook-Air.local>
2019-06-09 21:21:23 -0300

    Adding modules and packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './prayers/prayers.module#PrayersModule'
+  },
+  {
+    path: 'prayers',
+    loadChildren: './prayers/prayers.module#PrayersModule'
+  },
+  {
+    path: 'admin',
+    loadChildren: './admin/admin.module#AdminModule'
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
